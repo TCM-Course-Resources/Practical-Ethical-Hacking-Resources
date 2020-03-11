5289acd6b45e5349550bf748989dca1b62b274c3
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-03-10 21:13:01 -0400

    Create strings.py

---
+++ python/strings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#print strings
+print("Hello, World!") #double quotes
+print("\n") # new line
+print('Hello, World!') #single quotes
+
+print (""" This string runs 
+multiple lines """) # triple quote for multi line 
+
+print("This is "+"a string") # we can also concatenate, this is combining variables in a string
