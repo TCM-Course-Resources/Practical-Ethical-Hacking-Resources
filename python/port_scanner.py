b9fa4f26559afd849ec9075ddd52e5fe80287120
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-03-11 00:09:44 -0400

    Update port_scanner.py

--- python/port_scanner.py
+++ python/port_scanner.py
@@ -15,7 +15,7 @@ print("Time started: " + str(dt,now()))
 print('-' * 50)
 
 try:
-  for port in range(1,10000):
+  for port in range(1,65535):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(0.5)
     result = s.connect_ex((target, port))
