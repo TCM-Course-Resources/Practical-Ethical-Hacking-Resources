f90b37cb3e623e608198b0ae569af6aceded091e
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-03-10 21:11:47 -0400

    Create math.py

---
+++ python/math.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print(50 + 50) # add
+print (50 - 50) # subtract
+print(50 * 50) # multiply
+print( 50 / 50) #divide
+print (50 + 50 - 50 * 50 / 50) # PEMDAS
+print (50 ** 2) #raising to power. exponents
+print (50 % 6) #modulo, get remainder
+print( 50 // 6) # divides w/ no remainder
