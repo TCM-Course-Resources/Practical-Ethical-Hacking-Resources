8c41447b5bdf92ec1a0543bc3bb2b328bdac669e
Grimmz <57014148+thatonetester@users.noreply.github.com>
2020-03-10 21:24:01 -0400

    Create script.py

---
+++ python/script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Variables and modules
+quote = "All is fair in love and war"
+print(quote.upper()) # uppercase
+print(quote.lower()) # lowercase
+print(quote.title()) # title
+
+print(len(quote)) # gets length of quote
+
+name = "Heath" # string
+age = 30 # int
+gpa = 3.7 # float
+
+print(int(age)) # casting int as int
+print(int(30.9)) # casting float as int
+
+print("Hello, my name is " + name + " and I am " + str(age) " years old!")
+
+age+=1
+print(age)
+
+birthday = 1
+age += birthday
+print(age)
