3982c63afd140e5f5fea85206250db8fff0ccf20
Amit Agarwal <redacted>
2022-03-06 16:11:12 +0530

    Modified for some colorful output.

--- bash/webrecon.sh
+++ bash/webrecon.sh
@@ -1,75 +1,74 @@
 #!/bin/bash
-
+# Modified by https://github.com/raj77in
+if [[ $# -le 0 ]]
+then
+	echo "Usage : $0 <Domain>"
+	exit 1
+fi
 url=$1
+
+## Color codes
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+
+
+
 if [ ! -d "$url" ];then
     mkdir $url
 fi
-if [ ! -d "$url/recon" ];then
-    mkdir $url/recon
-fi
-#    if [ ! -d '$url/recon/eyewitness' ];then
-#        mkdir $url/recon/eyewitness
-#    fi
-if [ ! -d "$url/recon/scans" ];then
-    mkdir $url/recon/scans
-fi
-if [ ! -d "$url/recon/httprobe" ];then
-    mkdir $url/recon/httprobe
-fi
-if [ ! -d "$url/recon/potential_takeovers" ];then
-    mkdir $url/recon/potential_takeovers
-fi
-if [ ! -d "$url/recon/wayback" ];then
-    mkdir $url/recon/wayback
-fi
-if [ ! -d "$url/recon/wayback/params" ];then
-    mkdir $url/recon/wayback/params
-fi
-if [ ! -d "$url/recon/wayback/extensions" ];then
-    mkdir $url/recon/wayback/extensions
-fi
-if [ ! -f "$url/recon/httprobe/alive.txt" ];then
-    touch $url/recon/httprobe/alive.txt
-fi
-if [ ! -f "$url/recon/final.txt" ];then
-    touch $url/recon/final.txt
-fi
+
+for i in recon/eyewitness recon/scans recon/httprobe recon/potential_takeovers recon/wayback recon/wayback/params recon/wayback/extensions
+do
+	dir=$url/$i
+	[[ ! -d $dir ]] && mkdir -p $dir
+done
+
+function header()
+{
+    echo "## ================================================== ##"
+    echo -e "[${green}+${reset}] Starting $blue$1$reset to $blue'$2'$reset ..."
+    echo "## ================================================== ##"
+}
+
+header assetfinder  "Harvest subdomains"
+assetfinder $url | grep $url >> $url/recon/final.txt
  
-echo "[+] Harvesting subdomains with assetfinder..."
-assetfinder $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-rm $url/recon/assets.txt
- 
-#echo "[+] Double checking for subdomains with amass..."
+#header amass "Double checking"
 #amass enum -d $url >> $url/recon/f.txt
 #sort -u $url/recon/f.txt >> $url/recon/final.txt
 #rm $url/recon/f.txt
  
-echo "[+] Probing for alive domains..."
-cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/a.txt
+header httprobe "Check alive domains"
+cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443'  >> $url/recon/httprobe/a.txt
 sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
 rm $url/recon/httprobe/a.txt
  
 echo "[+] Checking for possible subdomain takeover..."
  
-if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
-    touch $url/recon/potential_takeovers/potential_takeovers.txt
-fi
- 
+header subjack "Possible Domain TakeOver" 
 subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
  
-echo "[+] Scanning for open ports..."
+header nmap "Port Scan"
 nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
  
-echo "[+] Scraping wayback data..."
+header waybackurls "Wayback data"
 cat $url/recon/final.txt | waybackurls >> $url/recon/wayback/wayback_output.txt
 sort -u $url/recon/wayback/wayback_output.txt
  
-echo "[+] Pulling and compiling all possible params found in wayback data..."
+header params "Scraping params"
 cat $url/recon/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f 1 | sort -u >> $url/recon/wayback/params/wayback_params.txt
 for line in $(cat $url/recon/wayback/params/wayback_params.txt);do echo $line'=';done
  
-echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
+header Scraping "Pulling and compiling js/php/aspx/jsp/json files"
 for line in $(cat $url/recon/wayback/wayback_output.txt);do
     ext="${line##*.}"
     if [[ "$ext" == "js" ]]; then
@@ -101,3 +100,4 @@ rm $url/recon/wayback/extensions/php1.txt
 rm $url/recon/wayback/extensions/aspx1.txt
 #echo "[+] Running eyewitness against all compiled domains..."
 #python3 EyeWitness/EyeWitness.py --web -f $url/recon/httprobe/alive.txt -d $url/recon/eyewitness --resolve
+
